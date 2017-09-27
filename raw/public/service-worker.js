432d72d6a9ecb01df118767e50194d4dbcf3c5a1
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-09-27 20:50:12 +0700

    Update cached file

--- raw/public/service-worker.js
+++ raw/public/service-worker.js
@@ -13,7 +13,23 @@ self.addEventListener('install', function (event) {
         caches.open('t-cache').then(function (cache) {
             // And add resources to it
             return cache.addAll([
-                'manifest.min.json'
+                '/service-worker.min.js',
+                '/manifest.min.json',
+                '/assets/vendor/bootstrap/dist/css/bootstrap.min.css',
+                '/assets/vendor/bootstrap/dist/css/bootstrap-theme.min.css',
+                '/assets/vendor/ionicons/dist/css/ionicons.min.css',
+                '/assets/vendor/font-awesome/css/font-awesome.min.css',
+                '/assets/vendor/admin-lte/dist/css/AdminLTE.min.css',
+                '/assets/vendor/admin-lte/dist/css/skins/skin-black.min.css',
+                '/assets/vendor/html5-boilerplate/dist/css/normalize.min.css',
+                '/assets/vendor/html5-boilerplate/dist/css/main.min.css',
+                '/assets/vendor/html5-boilerplate/dist/js/vendor/modernizr-3.5.0.min.js',
+                '/assets/vendor/jquery/dist/jquery.min.js',
+                '/assets/vendor/html5-boilerplate/dist/js/plugins.min.js',
+                '/assets/vendor/bootstrap/dist/js/bootstrap.min.js',
+                '/assets/vendor/fastclick/lib/fastclick.min.js',
+                '/assets/vendor/admin-lte/dist/js/adminlte.min.js',
+                '/assets/js/common/service-worker-logging.min.js'
             ]);
         })
     );
