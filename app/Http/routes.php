d75b9e9df79fba12f3ad9c85a4c2018e594e0346
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-01 19:52:44 +0700

    Completing lesson4

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,9 @@ $router->get('/template/boilerplate', 'TemplateController@boilerplate');
 $router->get('/template/bootstrap', 'TemplateController@bootstrap');
 $router->get('/template/adminlte', 'TemplateController@adminlte');
 $router->get('/template/semantic', 'TemplateController@semantic');
+$router->get('/lesson4/direct', 'Lesson4Controller@direct');
+$router->get('/lesson4/response', 'Lesson4Controller@response');
+$router->get('/lesson4/json', 'Lesson4Controller@json');
+$router->get('/lesson4/closure', function () {
+    return 'This is closure';
+});
