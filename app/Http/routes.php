bfa0b1ba776ff1dc5d56440ac5f4f2b91c465dd0
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-02 08:39:39 +0700

    Update route binding

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,13 +14,18 @@
 
 /** @var \Illuminate\Routing\Router $router */
 $router->get('/', 'WelcomeController@index');
+/*Template*/
 $router->get('/template/boilerplate', 'TemplateController@boilerplate');
 $router->get('/template/bootstrap', 'TemplateController@bootstrap');
 $router->get('/template/adminlte', 'TemplateController@adminlte');
 $router->get('/template/semantic', 'TemplateController@semantic');
+/*Lesson4*/
 $router->get('/lesson4/direct', 'Lesson4Controller@direct');
 $router->get('/lesson4/response', 'Lesson4Controller@response');
 $router->get('/lesson4/json', 'Lesson4Controller@json');
-$router->get('/lesson4/closure', function () {
+$router->get('/lesson4/closure/direct', function () {
     return 'This is closure';
+});
+$router->get('/lesson4/closure/view', function () {
+    return view('layout.lesson4.response.lesson4_response_default');
 });
