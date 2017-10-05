106c57a84ac3a6395184d6d505361c43e75b9731
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-05 09:26:30 +0700

    Add songs controller

---
+++ app/Http/Controllers/S01/E06/SongsController.php
@@ -0,0 +1,60 @@
+<?php namespace App\Http\Controllers\S01\E06;
+
+use App\Http\Controllers\Controller;
+
+class SongsController extends Controller
+{
+    private $songs;
+
+    /**
+     * SongsController constructor.
+     */
+    public function __construct()
+    {
+        $this->songs = [
+            'Song 1',
+            'Song 2',
+            'Song 3',
+            'Song 4',
+            'Song 5',
+        ];
+    }
+
+
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function songList()
+    {
+        $songs = $this->bucket();
+
+        return view('layout.s01.e06.songlist.s01_e06_songlist_default', compact('songs'));
+    }
+
+    /**
+     * @param int|null $id
+     * @return array|string
+     */
+    private function bucket($id = null)
+    {
+        if (is_null($id))
+        {
+            return $this->songs;
+        }
+        else
+        {
+            return $this->songs[$id];
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\View\View
+     */
+    public function songGet($id)
+    {
+        $song = $this->bucket($id);
+
+        return view('layout.s01.e06.songget.s01_e06_songget_default', compact('song'));
+    }
+}
