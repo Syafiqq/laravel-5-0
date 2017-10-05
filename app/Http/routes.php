9b251758c995770a84594068c2e970a51d7d454d
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-05 09:26:41 +0700

    Add router for song

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,8 @@ $router->get('/template/boilerplate', 'TemplateController@boilerplate');
 $router->get('/template/bootstrap', 'TemplateController@bootstrap');
 $router->get('/template/adminlte', 'TemplateController@adminlte');
 $router->get('/template/semantic', 'TemplateController@semantic');
+//Lesson 006
+$router->group(['prefix' => 's01/e06'], function () use ($router) {
+    $router->get('/songs', 'S01\E06\SongsController@songList');
+    $router->get('/songs/{id}', 'S01\E06\SongsController@songGet')->where('id', '[0-9]+');
+});
