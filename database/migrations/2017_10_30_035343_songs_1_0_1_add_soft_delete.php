004986591d2845f6b6b1eebd3760f6d3c31774e0
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-30 10:58:12 +0700

    Add soft delete
    (cherry picked from commit 44da6a0)

---
+++ database/migrations/2017_10_30_035343_songs_1_0_1_add_soft_delete.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class Songs101AddSoftDelete extends Migration
+{
+    /**
+     * @var Illuminate\Database\Schema\Builder
+     */
+    private $schema;
+    /**
+     * @var string
+     */
+    private $tableName;
+
+    /**
+     * Songs100 constructor.
+     */
+    public function __construct()
+    {
+        $this->schema    = \Illuminate\Support\Facades\Schema::getFacadeRoot();
+        $this->tableName = 'songs';
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if ($this->schema->hasTable($this->tableName))
+        {
+            $this->schema($this->tableName, function (Blueprint $table) {
+                $table->softDeletes();
+            });
+        }
+        else
+        {
+            echo "Table $this->tableName doesn't exists";
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if ($this->schema->hasTable($this->tableName))
+        {
+            $this->schema($this->tableName, function (Blueprint $table) {
+                $table->dropSoftDeletes();
+            });
+        }
+        else
+        {
+            echo "Table $this->tableName doesn't exists";
+        }
+    }
+
+}
